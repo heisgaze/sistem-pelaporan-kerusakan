eaa6b0710c8c8e6435372c1cb0f76e81d093af62
Galang Rizky <galangrizky@Galangs-MacBook-Air.local>
2025-12-20 13:46:44 +0700

    setup awal + auth

---
+++ README.md
@@ -0,0 +1,15 @@
+# Progress
+## bagian Galang 
+- setup laravel
+- setup sanctum (Token authorization)
+- setup database (migrations)
+- setup controller auth + test postman
+- setup middleware
+- create model user
+- create endpoint auth (register, login, logout)
+
+## bagian Lola
+
+## bagian Sabila
+
+## bagian Luthfi
---
+++ database/migrations/2025_12_20_060427_create_pengumuman_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengumuman', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('konten');
+
+            $table->enum('status', ['draft', 'aktif', 'arsip'])
+                ->default('draft');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengumuman');
+    }
+};
