f9a90009012f07d83085e5afc750d6c605cf40a6
Galang Rizky <galangmazaya001@gmail.com>
2026-05-19 17:10:25 +0700

    fix: force https scheme in production to fix mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 
 
@@ -21,7 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         View::share('unreadNotifications', 0);
     }
 }
